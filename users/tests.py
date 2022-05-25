70502d9700b00ededbdf457b98c6b23cdb091031
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-05-25 17:44:48 +0900

    Add : user signup views and tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,106 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test  import Client, TransactionTestCase
+from users.models import User
+
+class UserSignupTest(TransactionTestCase):
+    def setUp(self):
+        User.objects.create_user(
+            name = 'test1',
+            email = 'test1@gmail.com',
+            is_doctor = 'False',
+            password = '1q2w3e4r'
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
+    
+    def test_signup_success(self):
+        client = Client()
+        form = {
+            'name'  : 'test2',
+            'email' : 'test2@gmail.com',
+            'is_doctor' : 'False',
+            'password' : '1q2w3e4r'
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'message' : 'signup success'})
+    
+    def test_email_already_exist(self):
+        client = Client()
+        form = {
+            'name'  : 'test1',
+            'email' : 'test1@gmail.com',
+            'is_doctor' : 'False',
+            'password' : '1q2w3e4r'
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'email is already exists'})
+
+    def test_keyerror(self):
+        client = Client()
+        form = {
+            'name'  : 'test2',
+            'email' : 'test2@gmail.com',
+            'is_doctor' : 'False',
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'KeyError'})
+
+    def test_name_is_none(self):
+        client = Client()
+        form = {
+            'name'  : '',
+            'email' : 'test2@gmail.com',
+            'is_doctor' : 'False',
+            'password' : '1q2w3e4r'
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'must have user name'})
+    
+    def test_email_is_none(self):
+        client = Client()
+        form = {
+            'name'  : 'test2',
+            'email' : '',
+            'is_doctor' : 'False',
+            'password' : '1q2w3e4r'
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'must have user email'})
+    
+    def test_is_doctor_is_none(self):
+        client = Client()
+        form = {
+            'name'  : 'test2',
+            'email' : 'test2@gmail.com',
+            'is_doctor' : '',
+            'password' : '1q2w3e4r'
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'must have user is_doctor'})
+    
+    def test_password_is_none(self):
+        client = Client()
+        form = {
+            'name'  : 'test2',
+            'email' : 'test2@gmail.com',
+            'is_doctor' : 'False',
+            'password' : ''
+        }	
+	    
+        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'must have user password'})
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from users.models    import User
+from django.views    import View
+from django.http     import JsonResponse
+from django.db.utils import IntegrityError
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data      = json.loads(request.body)
+            name      = data['name']
+            email     = data['email']
+            is_doctor = data['is_doctor']
+            password  = data['password']
+
+            User.objects.create_user(
+                name      = name,
+                password  = password,
+                email     = email,
+                is_doctor = is_doctor
+            )
+            return JsonResponse({'message' : 'signup success'}, status = 201)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KeyError'}, status = 400)
+        except IntegrityError:
+            return JsonResponse({'message' : 'email is already exists'}, status = 400)
+        except ValueError as e:
+            return JsonResponse({'message' : f'{e}'}, status = 400)
