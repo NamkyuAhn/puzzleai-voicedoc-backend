e5b7360f5cce240ab5d6657ee32e889f08774fe9
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-05-26 19:40:15 +0900

    Add : patient decorator

--- core/decorator.py
+++ core/decorator.py
@@ -13,3 +13,19 @@ def signin_decorator(func):
         except KeyError:
             return JsonResponse({'message' : 'signin time expired'})
     return wrapper
+
+def patient_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            user_id = request.session['user_id']
+            user = User.objects.get(id=user_id)
+            if request.user.is_doctor == True : 
+                return False
+            return func(self, request, *args, **kwargs)
+
+        except User.DoesNotExist:
+            return JsonResponse({'message':'IVALID_USER'},status=401)
+        except KeyError:
+            return JsonResponse({'message' : 'signin time expired'})
+    return wrapper
+
