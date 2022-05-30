ebcee8a65c20aed6208f33a6022bddac5711e76e
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-05-30 15:22:40 +0900

    Edit : deleted validation logic, session changed to jwt

--- core/functions.py
+++ core/functions.py
@@ -1,5 +1,19 @@
+import jwt
+import datetime
+
 from django.http  import JsonResponse
-from users.models import User
+
+from users.models      import User
+from voicedoc.settings import SECRET, ALGORITHM
+
+def jwt_generator(payload, user_id):
+    payload['user_id'] = user_id
+    encoded = jwt.encode(payload, SECRET, algorithm = ALGORITHM)
+    return encoded
+
+def jwt_decoder(token):
+    decoded = jwt.decode(token, SECRET, algorithms = ALGORITHM)
+    return decoded
 
 def signin_decorator(func):
     def wrapper(self, request, *args, **kwargs):
@@ -9,7 +23,7 @@ def signin_decorator(func):
             return func(self, request, *args, **kwargs)
 
         except User.DoesNotExist:
-            return JsonResponse({'message':'IVALID_USER'},status=401)
+            return JsonResponse({'message' : 'IVALID_USER'},status=401)
         except KeyError:
             return JsonResponse({'message' : 'signin time expired'})
     return wrapper
@@ -18,13 +32,13 @@ def patient_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             user_id = request.session['user_id']
-            user = User.objects.get(id=user_id)
+            request.user = User.objects.get(id=user_id)
             if request.user.is_doctor == True : 
-                return False
+                return JsonResponse({'message': "doctor can't access to patient menu"}, status = 403)
             return func(self, request, *args, **kwargs)
 
         except User.DoesNotExist:
-            return JsonResponse({'message':'IVALID_USER'},status=401)
+            return JsonResponse({'message' : 'IVALID_USER'}, status=401)
         except KeyError:
             return JsonResponse({'message' : 'signin time expired'})
     return wrapper
--- users/tests.py
+++ users/tests.py
@@ -66,32 +66,6 @@ class UserSignupTest(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message' : 'must have user is_doctor'})
     
-    def test_email_validation_error(self):
-        client = Client()
-        form = {
-            'name'      : 'test2',
-            'email'     : 'test2gmail.com',
-            'is_doctor' : 'False',
-            'password'  : '1q2w3e4r'
-        }	
-	    
-        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'not in email format'})
-    
-    def test_password_validation_error(self):
-        client = Client()
-        form = {
-            'name'      : 'test2',
-            'email'     : 'test2@gmail.com',
-            'is_doctor' : 'False',
-            'password'  : '1234'
-        }	
-	    
-        response = client.post('/users/signup', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'not in password format'})
-
 class UserSignupIntergrityErrorTest(TransactionTestCase):
     def setUp(self):
         User.objects.create_user(
@@ -117,52 +91,39 @@ class UserSignupIntergrityErrorTest(TransactionTestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message' : 'email is already exists'})
 
-class UserValidationTest(TestCase):
-    def test_email_validation_pass(self):
-        client = Client()
-        form = {'email' : 'test1@gmail.com'}
+class EmailUniqueTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            name = 'test',
+            email = 'test1@gmail.com',
+            password = '1q2w3e4r',
+            is_doctor = 'False'
+        )
 
-        response = client.post('/users/email_check', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'message' : 'email validation pass'})
-
-    def test_email_validation_error(self):
-        client = Client()
-        form = {'email' : 'test2gmail.com'}	
-	    
-        response = client.post('/users/email_check', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'not in email format'})
+    def tearDown(self):
+        User.objects.all().delete()
     
-    def test_email_validation_keyerror(self):
+    def test_email_uniquetest_pass(self):
+        client = Client()
+        form   = {'email' : 'test2@gmail.com'}
+
+        response = client.post('/users/email_check', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message' : 'email unique check pass'})
+    
+    def test_email_uniquetest_fail(self):
+        client = Client()
+        form   = {'email' : 'test1@gmail.com'}
+
+        response = client.post('/users/email_check', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message' : 'email already exists'})
+    
+    def test_email_uniquetest_keyerror(self):
         client = Client()
         form = {}	
 	    
         response = client.post('/users/email_check', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'KeyError'})
-
-    def test_password_validation_pass(self):
-        client = Client()
-        form = {'password' : '1q2w3e4r'}	
-	    
-        response = client.post('/users/password_check', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {'message' : 'password validation pass'})
-
-    def test_password_validation_error(self):
-        client = Client()
-        form = {'password' : '1234'}	
-	    
-        response = client.post('/users/password_check', json.dumps(form), content_type='application/json')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'not in password format'})
-
-    def test_password_validation_keyerror(self):
-        client = Client()
-        form = {}	
-	    
-        response = client.post('/users/password_check', json.dumps(form), content_type='application/json')
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message' : 'KeyError'})
 
@@ -200,10 +161,18 @@ class UserSigninTest(TestCase):
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message' : 'check email or password'})
 
-    def test_doctor_signin(self):#웹 앱 구분?
-        client = Client()
+    def test_doctor_signin_success(self):#웹 앱 구분?
+        client = Client(HTTP_USER_AGENT='Chrome/101.0.4951.64')
+        form = {'email' : 'doctor@gmail.com', 'password' : '1q2w3e4r'}
+
+        response = client.post('/users/signin', json.dumps(form), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {'message' : 'signin success for doctor'})
+
+    def test_doctor_signin_fail(self):
+        client = Client(HTTP_USER_AGENT='Darwin')
         form = {'email' : 'doctor@gmail.com', 'password' : '1q2w3e4r'}
 
         response = client.post('/users/signin', json.dumps(form), content_type='application/json')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(), {'message' : 'please signin on app for doctor'})
+        self.assertEqual(response.json(), {'message' : 'signin failed because you are not patient'})
