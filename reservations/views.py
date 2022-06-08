51a7bfb199fc3b965e6d208aee1c622f46047311
NamkyuAhn <mkdir.nkahn@gmail.com>
2022-06-08 15:13:35 +0900

    Edit : removed day query string

--- reservations/views.py
+++ reservations/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import date
 
 from reservations.models import Reservation
 from users.models        import Subject, Doctor, DoctorDay, DoctorTime
@@ -6,7 +6,7 @@ from core.functions      import signin_decorator
 from voicedoc.settings   import IP_ADDRESS
 
 from django.views import View
-from django.http  import JsonResponse
+from django.http  import JsonResponse, HttpResponse
 from django.db.models.functions import Concat
 from django.db.models           import CharField, Value
 
@@ -20,7 +20,7 @@ class SubjectView(View):
                 )
             )\
         .values('id', 'name', 'file_location')        
-        return JsonResponse({"result" : list(subjects)}, status = 200)
+        return JsonResponse({"result" : list(subjects), "name" : request.user.name}, status = 200)
 
 class DoctorListView(View):
     @signin_decorator
@@ -39,25 +39,35 @@ class DoctorListView(View):
 
 class DoctorWorkView(View):
     def get(self, request, doctor_id):
-        today = datetime.date.today()
-        year  = request.GET.get('year', str(today.year))
-        month = request.GET.get('month', str(today.month))
-        day   = request.GET.get('day', None)
-        date = request.GET.get('date', None)
-        if (date != None) and day != None:
-            date = f'{year}-{month}-{date}'
-            reservations = Reservation.objects.filter(doctor_id = doctor_id, date = date)
-            working_time = DoctorTime.objects.get(days = day).times
-            time_list = working_time.split(',')
-            working_times = {'times' : time_list}
-            expired_time = [reservation.time for reservation in reservations]
-            return JsonResponse({'working_times' : working_times,
-                                 'expired_times' : expired_time}, status = 200)
+        try:
+            year   = request.GET.get('year')
+            month  = request.GET.get('month')
+            dates  = request.GET.get('dates', None)
 
-        working_day = DoctorDay.objects.get(doctor_id = doctor_id, 
-                                year = year, month = month).days
-        time_list = working_day.replace('"','').split(",")
-        return JsonResponse({'result' : time_list}, status = 200)
+            if dates != None: 
+                full_day = f'{year}-{month}-{dates}'
+                reservations = Reservation.objects.filter(doctor_id = doctor_id, date = full_day)
+                day = date(int(year), int(month), int(dates)).weekday()
+                working_time = DoctorTime.objects.get(days = day).times
+                time_list = working_time.split(',')
+                working_times = {'times' : time_list}
+                expired_time = [reservation.time for reservation in reservations]
+                return JsonResponse({'working_times' : working_times,
+                                    'expired_times' : expired_time}, status = 200)
+
+            working_day = DoctorDay.objects.get(doctor_id = doctor_id, 
+                                    year = year, month = month).days
+            time_lists = working_day.split(",")
+            result = []
+            for time in time_lists:
+                result.append(int(time))
+            return JsonResponse({'result' : result}, status = 200)
+
+        except DoctorDay.DoesNotExist:
+            return HttpResponse("조건에 부합하는 의사 근무날짜 없음")
+
+        except DoctorTime.DoesNotExist:
+            return HttpResponse("조건의 부합하는 의사 근무시간 없음")
               
                     
 
